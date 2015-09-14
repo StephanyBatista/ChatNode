61a0685d3b4042f00d4c42b428c0608b8565d79c
StephanyBatista <stephany.batista@outlook.com>
2015-09-14 15:22:12 -0400

    alter home.js to get and not "use"

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
 
 module.exports = function(app){
   
   var controller = app.controllers.home;
-  app.use('/', router.get('/', controller.index));
-  app.use('/logon', router.get('/logon', controller.logon));
+  app.get('/', controller.index);
+  app.get('/logon', controller.logon);
 }
